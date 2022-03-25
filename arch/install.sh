908eef147f3d4b9a1683936c7820ec07074162aa
Kieren Davies <kieren.davies@zivver.com>
2022-03-25 17:19:52 +0100

    Install paru

---
+++ arch/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR=$(dirname "$0")
+
+sudo pacman -S --needed base-devel git
+
+if ! pacman -Qi paru > /dev/null; then
+    echo "Installing paru"
+
+    if [ -n "$XDG_CACHE_HOME" ]; then
+        PARU_CACHE_DIR="$XDG_CACHE_HOME/paru"
+    else
+        PARU_CACHE_DIR="$HOME/.cache/paru"
+    fi
+    PARU_REPO_DIR="$PARU_CACHE_DIR/clone/paru-bin"
+
+    if [ ! -d "$PARU_REPO_DIR/.git" ]; then
+        git clone https://aur.archlinux.org/paru-bin.git "$PARU_REPO_DIR"
+    fi
+    pushd "$PARU_REPO_DIR"
+    git pull
+    makepkg -sic
+    popd
+fi
