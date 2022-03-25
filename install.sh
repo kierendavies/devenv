3c95c5dab9dcad21da53c8a2ab4c0a03507ddd8d
Kieren Davies <kieren.davies@zivver.com>
2022-03-25 19:33:08 +0100

    Configure fish

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+if [ -z "$XDG_CACHE_HOME" ]; then
+    export XDG_CACHE_HOME="$HOME/.cache"
+fi
+if [ -z "$XDG_DATA_HOME" ]; then
+    export XDG_DATA_HOME="$HOME/.local/share"
+fi
+
 if [ -f "/etc/arch-release" ]; then
     arch/install.sh
 fi
