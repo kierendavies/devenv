df871f454ce40b5f4dd569140c3b1f1c0d1b453b
Kieren Davies <kieren@kdavi.es>
2022-03-27 20:27:25 +0200

    Consolidate into one script

--- install.sh
+++ install.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+COMMON_SYSTEM_PACKAGES=(
+    bat
+    curl
+    fish
+    fzf
+    grc
+    stow
+    thefuck
+)
+
 STOW_PACKAGES=(
     fish
 )
@@ -15,11 +25,33 @@ if [ -z "$XDG_DATA_HOME" ]; then
     export XDG_DATA_HOME="$HOME/.local/share"
 fi
 
-if [ -f /etc/arch-release ]; then
-    arch/install.sh
-elif grep -q 'ID=ubuntu' /etc/os-release; then
-    ubuntu/install.sh
-fi
+OS_ID=$(grep '^ID=' /etc/os-release | cut -d= -f2)
+echo "Detected OS: $OS_ID"
+
+echo "Installing system packages"
+case $OS_ID in
+arch)
+    sudo pacman -Sy --needed base-devel git
+
+    if ! pacman -Qi paru >/dev/null; then
+        echo "Installing paru from AUR"
+        PARU_REPO_DIR="$XDG_CACHE_HOME/paru/clone/paru-bin"
+        if [ ! -d "$PARU_REPO_DIR/.git" ]; then
+            git clone https://aur.archlinux.org/paru-bin.git "$PARU_REPO_DIR"
+        fi
+        pushd "$PARU_REPO_DIR"
+        git pull
+        makepkg -sic
+        popd
+    fi
+
+    paru -S --needed "${COMMON_SYSTEM_PACKAGES[@]}"
+    ;;
+ubuntu)
+    sudo apt update
+    sudo apt install -y ${COMMON_SYSTEM_PACKAGES[@]}
+    ;;
+esac
 
 # Don't use $SHELL because it is only updated at next login
 DEFAULT_SHELL=$(getent passwd $(id -un) | cut -d : -f 7-)
