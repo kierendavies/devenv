e00399abb04213edf05db63cecaadb5f38c321e0
Kieren Davies <kieren@kdavi.es>
2022-04-24 17:35:15 +0200

    Use type instead of command

--- stow/fish/.config/fish/conf.d/aliases.fish
+++ stow/fish/.config/fish/conf.d/aliases.fish
@@ -2,11 +2,11 @@
 alias diff='diff --color=auto'
 alias grep='grep --color=auto'
 alias ip='ip -color=auto'
-if command -q lsd
+if type -q lsd
     alias ls lsd
     alias tree 'lsd --tree'
 end
 
 # Shortcuts
 alias ll 'ls -lAh' # Default doesn't do -A
-command -q nvim; and alias vim nvim
+type -q nvim; and alias vim nvim
