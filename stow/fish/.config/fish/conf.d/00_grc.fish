12b90889fa144f18dd9416ee14b096b6437572d3
Kieren Davies <kieren@kdavi.es>
2022-05-01 12:08:54 +0200

    Wrap more commands with grc

--- stow/fish/.config/fish/conf.d/00_grc.fish
+++ stow/fish/.config/fish/conf.d/00_grc.fish
@@ -1,2 +1,62 @@
-# Exclude ls, because piping makes it emit one entry per line.
-set -U grc_plugin_execs cat cvs df diff dig gcc g++ ifconfig make mount mtr netstat ping ps tail traceroute wdiff
+# Must be evaluated before grc.fish, provided by omf package
+# List copied from /etc/grc.fish
+
+set -g grc_plugin_execs \
+    blkid \
+    cat \
+    cvs \
+    df \
+    diff \
+    dig \
+    dnf \
+    docker \
+    docker-compose \
+    docker-machine \
+    du \
+    env \
+    fdisk \
+    findmnt \
+    free \
+    g++ \
+    gcc \
+    getfacl \
+    getsebool \
+    id \
+    ifconfig \
+    iostat \
+    ip \
+    journalctl \
+    kubectl \
+    last \
+    # ls \ # Alias to lsd instead
+    lsattr \
+    lsblk \
+    lsmod \
+    lsof \
+    lspci \
+    make \
+    mount \
+    mtr \
+    netstat \
+    nmap \
+    ping \
+    ps \
+    sar \
+    semanage \
+    showmount \
+    sockstat \
+    ss \
+    stat \
+    sysctl \
+    systemctl \
+    tail \
+    tcpdump \
+    traceroute \
+    tune2fs \
+    ulimit \
+    uptime \
+    vmstat \
+    w \
+    wdiff \
+    who \
+    # END - keep the last slash
